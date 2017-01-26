08145b44dd9a994a5624d766b32cab4d02b14688
alisonburgers <alisonburgers007@gmail.com>
2017-01-26 22:04:15 +0000

    Delete popup.js

--- pack/popup.js
+++
@@ -1,5 +0,0 @@
-var button = document.getElementById("mybutton");
-button.addEventListener("click", function() {
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
-  window.close();
-}, false);
